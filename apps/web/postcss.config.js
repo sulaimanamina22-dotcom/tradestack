dbc408b7a252365e50df53f02bf7a2bb83fc0e54
Sulaiman <sulaimanamina22@gmail.com>
2025-12-31 07:16:30 -0800

    fix: revert to standard postcss string config

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,11 +1,6 @@
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
-const config = {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-  ],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
